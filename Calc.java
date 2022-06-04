6a5c656bb043998ccf5302e1f785184d202accd8
yundongjun <ydjcodename@gmail.com>
2022-06-04 14:08:00 +0900

    수정

--- Calc.java
+++ Calc.java
@@ -3,5 +3,6 @@ public Class Calc{
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("Calc");
 	}
 }
--- import/Add.java
+++ import/Add.java
@@ -1,2 +1,4 @@
 public class Add{
+	public static void main(String[] args){
+	}
 }
